39971eba9ef1e689f09fe5dc5793cf646be6f51e
ashutoshmishra5 <138553330+ashutoshmishra5@users.noreply.github.com>
2024-06-04 07:41:11 +0530

    Update route.js

--- src/app/api/blogApi/route.js
+++ src/app/api/blogApi/route.js
@@ -1,22 +1,20 @@
 import mongoose from "mongoose";
 import { Blog } from "../../lib/models";
 
-async function connectDb() {
+const connectDb = async () => {
   if (mongoose.connection.readyState) return;
   await mongoose.connect(process.env.DATABASE_URL1, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
-}
+};
 
 export async function GET() {
+  await connectDb();
   try {
-    await connectDb();
     const data = await Blog.find();
-    console.log(data);
     return new Response(JSON.stringify({ result: data }), { status: 200, headers: { "Content-Type": "application/json" } });
   } catch (error) {
-    console.error("Error retrieving data:", error);
     return new Response(JSON.stringify({ result: false, error: "Error retrieving data" }), { status: 500, headers: { "Content-Type": "application/json" } });
   }
 }
