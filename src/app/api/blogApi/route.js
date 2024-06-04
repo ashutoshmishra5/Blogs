6ea9853a2d911b9a9933f0e77e546312bb836200
ashutoshmishra5 <138553330+ashutoshmishra5@users.noreply.github.com>
2024-06-04 07:31:09 +0530

    Update route.js

--- src/app/api/blogApi/route.js
+++ src/app/api/blogApi/route.js
@@ -2,15 +2,23 @@ import mongoose from "mongoose";
 import { NextResponse } from "next/server";
 import { Blog } from "../../lib/models";
 
-export async function GET(){
+async function connectDb() {
+    if (mongoose.connection.readyState) return;
+    await mongoose.connect(process.env.DATABASE_URL1, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+  }
+
+export async function GET() {
     try {
-        await mongoose.connect(process.env.DATABASE_URL1);
-        const data = await Blog.find();
-        console.log(data);
-        return NextResponse.json({ result: data });
+      await connectDb();
+      const data = await Blog.find();
+      console.log(data);
+      return NextResponse.json({ result: data });
     } catch (error) {
-        console.error("Error retrieving data:", error);
-        return NextResponse.json({ result: false, error: "Error retrieving data" });
+      console.error("Error retrieving data:", error);
+      return NextResponse.json({ result: false, error: "Error retrieving data" });
     }
-    
-}
+  }
+  
