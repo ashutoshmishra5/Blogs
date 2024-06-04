a87798ee09e42a3c375171135f1eeae174c7ee12
ashutoshmishra5 <138553330+ashutoshmishra5@users.noreply.github.com>
2024-06-04 07:35:31 +0530

    Update route.js

--- src/app/api/blogApi/route.js
+++ src/app/api/blogApi/route.js
@@ -1,24 +1,22 @@
 import mongoose from "mongoose";
-import { NextResponse } from "next/server";
 import { Blog } from "../../lib/models";
 
 async function connectDb() {
-    if (mongoose.connection.readyState) return;
-    await mongoose.connect(process.env.DATABASE_URL1, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-  }
+  if (mongoose.connection.readyState) return;
+  await mongoose.connect(process.env.DATABASE_URL1, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+}
 
 export async function GET() {
-    try {
-      await connectDb();
-      const data = await Blog.find();
-      console.log(data);
-      return NextResponse.json({ result: data });
-    } catch (error) {
-      console.error("Error retrieving data:", error);
-      return NextResponse.json({ result: false, error: "Error retrieving data" });
-    }
+  try {
+    await connectDb();
+    const data = await Blog.find();
+    console.log(data);
+    return new Response(JSON.stringify({ result: data }), { status: 200, headers: { "Content-Type": "application/json" } });
+  } catch (error) {
+    console.error("Error retrieving data:", error);
+    return new Response(JSON.stringify({ result: false, error: "Error retrieving data" }), { status: 500, headers: { "Content-Type": "application/json" } });
   }
-  
+}
