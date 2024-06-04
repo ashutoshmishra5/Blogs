4715a1e16149330342263e72357d5b2a36a0aa25
ashutoshmishra5 <138553330+ashutoshmishra5@users.noreply.github.com>
2024-06-04 08:03:19 +0530

    Update route.js

--- src/app/api/blogApi/route.js
+++ src/app/api/blogApi/route.js
@@ -2,23 +2,25 @@ import mongoose from "mongoose";
 import { NextResponse } from "next/server";
 import { Blog } from "../../lib/models";
 
+// Define a flag variable to force dynamic serving
+export const dynamic = 'force-dynamic';
+
 async function connectDb() {
-  if (mongoose.connection.readyState) return;
-  await mongoose.connect(process.env.DATABASE_URL1, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+    if (mongoose.connection.readyState) return;
+    await mongoose.connect(process.env.DATABASE_URL1, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 }
 
-export async function GET(req, res) {
-  await connectDb();
-  try {
-    const data = await Blog.find();
-    // Use res.json instead of NextResponse.json
-    return res.status(200).json({ result: data });
-  } catch (error) {
-    console.error("Error retrieving data:", error);
-    // Use res.json instead of NextResponse.json
-    return res.status(500).json({ result: false, error: "Error retrieving data" });
-  }
+export async function GET() {
+    try {
+        await connectDb();
+        const data = await Blog.find();
+        console.log(data);
+        return NextResponse.json({ result: data });
+    } catch (error) {
+        console.error("Error retrieving data:", error);
+        return NextResponse.json({ result: false, error: "Error retrieving data" });
+    }
 }
