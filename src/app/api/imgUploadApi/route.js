be70bc8cd09a390421b43e5da533a5bd08577206
ashutoshmishra5 <138553330+ashutoshmishra5@users.noreply.github.com>
2024-06-28 00:32:37 +0530

    Update route.js

--- src/app/api/imgUploadApi/route.js
+++ src/app/api/imgUploadApi/route.js
@@ -1,21 +1,17 @@
 import { NextResponse } from "next/server";
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 
-	//const secretAccessKey1 = env(AWS_S3_SECRET_ACCESS_KEY);
-	//const accessKeyId1 = env(AWS_S3_ACCESS_KEY_ID);
 
 const s3Client = new S3Client({
 	region: process.env.AWS_S3_REGION,
-	//credentials: {
-	//	secretAccessKey:secretAccessKey1,
-	//	accessKeyId:accessKeyId1
-	//}
+	credentials: {
+		secretAccessKey: process.env.AWS_S3_SECRET_ACCESS_KEY,
+		accessKeyId: process.env.AWS_S3_ACCESS_KEY_ID,
+	}
 });
 
 async function uploadFileToS3(file, fileName) {
-
 	const fileBuffer = file;
-
 	const data = {
 		Bucket: process.env.AWS_S3_BUCKET_NAME,
 		Key: `${fileName}`,
@@ -27,10 +23,7 @@ async function uploadFileToS3(file, fileName) {
 	await s3Client.send(command);
 	
 	const objectUrl = `https://${process.env.AWS_S3_BUCKET_NAME}.s3.${process.env.AWS_S3_REGION}.amazonaws.com/${fileName}`;
-
 	return objectUrl;
-  
-
 }
 
 export async function POST(request) {
